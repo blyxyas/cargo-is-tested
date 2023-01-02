a86341088e2384c6588d322ba053bedcf2cf154f
blyxyas <blyxyas@gmail.com>
2023-01-02 02:34:19 +0100

    Fix `testing/main.rs`. 0 Warnings

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -3,10 +3,9 @@
 use is_tested::is_tested;
 use is_not_tested::is_not_tested;
 
-// #[is_tested("src/other.rs")]
-// #[is_not_tested]
-// fn main() {}
-
-fn x() {}
-
+#[is_tested("src/other.rs")]
+#[is_not_tested]
 fn main() {}
+
+#[allow(dead_code)]
+fn x() {}
